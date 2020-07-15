bbf17d1f404ea472f71759e55c83e1a5e47ecf86
Oieua <43945568+Oieua@users.noreply.github.com>
2020-07-15 20:02:26 +0200

    Update IRC - Go back to college.py

--- Root-me/IRC - Go back to college/IRC - Go back to college.py
+++ Root-me/IRC - Go back to college/IRC - Go back to college.py
@@ -3,15 +3,15 @@ import pyperclip
 import math
 import time
 
-pyautogui.click(456,1057)
+pyautogui.click(456,1057) #Position of the chat bar
 pyautogui.typewrite("!ep1")
 pyautogui.typewrite(["enter"])
 time.sleep(0.5)
-pyautogui.doubleClick(375,359)
+pyautogui.doubleClick(375,359) #Position of the received text (n1)
 pyautogui.hotkey('ctrl','c')
 root = int(pyperclip.paste())
 root = math.sqrt(root)
-pyautogui.doubleClick(420,357)
+pyautogui.doubleClick(420,357) #Position of the received text (n2)
 pyautogui.hotkey('ctrl','c')
 multiply = int(pyperclip.paste())
 value = root * multiply
