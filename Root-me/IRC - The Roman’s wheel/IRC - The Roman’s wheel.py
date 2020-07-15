ad8e490c4746140bff17a5d7e049a828de89e667
Oieua <43945568+Oieua@users.noreply.github.com>
2020-07-15 20:03:29 +0200

    Update IRC - The Roman’s wheel.py

--- Root-me/IRC - The Roman’s wheel/IRC - The Roman’s wheel.py
+++ Root-me/IRC - The Roman’s wheel/IRC - The Roman’s wheel.py
@@ -3,11 +3,11 @@ import pyperclip
 import time
 import codecs
 
-pyautogui.click(377,360)
+pyautogui.click(377,360) #Position of the chat bar
 pyautogui.typewrite("!ep3")
 pyautogui.typewrite(["enter"])
 time.sleep(0.5)
-pyautogui.tripleClick(375,359)
+pyautogui.tripleClick(375,359) #Position of the received text
 pyautogui.hotkey('ctrl','c')
 encoded = str(pyperclip.paste())
 decoded = str(codecs.encode(encoded,'rot_13'))
